c894d3f2255c58a054fc3725a3f7df60780c68b5
Julissa6966 <95084721+Julissa6966@users.noreply.github.com>
2021-11-26 13:23:00 +0000

    Create main.py

---
+++ lib/main.py
@@ -0,0 +1,202 @@
+import django
+import django.utils
+import os
+from django.shortcuts import render
+from django.utils import accounts
+from os import *
+
+client_application = [
+  client_name = "Vicegram",
+  client_description = "This Is A New Social Media Network Lol",
+  client_owner = "Julissa" # DO NOT MODIFY OR YOU WILL DESTROY THE ENTIRE APP.
+  client_id = "74832083729262943835393638353826376435292262728263726392262529252629272630274840594739383629#6133112vC"
+  ]
+
+class client_network(self, client_network, data):
+  def network(self):
+    
+    client_network = [
+      client_network_name === 'Vicegram UK',
+      client_network_location === 'United Kingdom',
+      client_network_speed === '468mbs',
+      client_network_version === '^6.00.84.21',
+      client_network_price === set(plan=["$4563.99"]).per(month);
+      client_network_next_bill === ''set(afterMonth[(1st)])==of(everyMonth=True)
+      client_payment_info === "/backend/payment-info.json",
+      ]
+      
+      # DO NOT MODIFY OR YOU'LL BE SURD & BROUGHT TO COURT & MORE.
+      # EXCEPTION MODIFIES CAN BE MADE ONLY BY DEVELOPERS
+
+class network(self, network, checks=True) {
+  network = network() {
+    def network(network.ctx.coll):
+      network = checks {
+        for app() {
+          if user === ''[network=False(self.connectivity.error(error="There Was A Error Within Your Connection"))];
+          connectivity === False()
+          self.end.app()
+          quit()
+        }
+        else:
+          if user === ''[network=True(self.open.app())]:
+            open = FEED(True):
+              data === network()
+      }
+  }
+}
+  
+class vicegram_start(start) {
+  def start() {
+    self.vicegram = new.application():
+      if vicegram === user.opens.application:
+        self.open();
+        loading_asset = "/assets/loading.mp4", data = [width={device.Width=True{set}}, height={device.Height=True{set}}]
+        asset.load = True() => {
+          asset_resolution = set_quality(set(texture_resolution="1080p", graphics="high")):
+            asset.load.data()
+          }
+          def app_data(self.app.data):
+            app = data = client.application()
+            applicationStart = True()
+            ANDROID.SYSTEM.START.APP() {
+              REGISTER()
+              d2 = ()/.self.fetch.database()
+            }
+            files_collect for start() {
+              "database/data/users/messages.json", "/database/data/users/VICEGRAM_USERS.json"
+              "android.kt",
+              "AndroidManifest.xml",
+              "application.yml",          
+            }
+            def window(self.data.window, window) {
+              data = [
+                caption_name = {"Vicegram"}
+                WIDTH = {device.width}
+                HIEGHT = {device.height}
+                TEXTURE_RESOLUTION = {"1080p"}
+                GRAPHIC_MODE = {"high"}
+                ]
+                django.window.create()
+                self.fetch.themeFromOtherPages("#000000, #ffffff, #00063F, FF0000, FF5282, F34271")
+            }
+        }]
+          load=True()
+          onLanuch=True()
+  }
+  VICEGRAM_MANIFEST = 1.00() {
+    def fetched_data(self, fetched_data=True):
+      data = [
+        fetch = ["django.all-auth.accounts", "social", "landing"]
+        fetch = ["django", "utils", "accounts", "admins", "feeds", "followers", "following", "feed"]
+      ]
+      self.fetch = data()
+  }
+  def loading(self, loading, load, intervals):
+    loading = True():
+      getExpectedVideoLoadingTime = True() {
+        self.gather = True;
+        if expected_time = done():
+          self.app.load()
+          else {
+            if over_expected_time = self.fetch.user{network=ctx.author.network.connectivity=True}
+            elif time(onload=True)
+          }
+          for app(self.app) = {
+            self.app.load = True();
+            data = collect(/self.data)==[]>LOAD()=TRUE()
+            django.collect=True()\{}[].data()=coll().certisize()
+            APP.LOAD.ALL.ASSETS()
+          }
+      }
+      WINDOW_CREATE()
+}
+
+class index(self, new.page, pageTitle="Feed") {
+  WINDOW = new.window [
+    window_data = [
+      screen.window_caption = "Feed";
+      screen.data = [width, height = ({device.Width || device.Height})];
+      texture_resolution = defaultRes();
+      graphic_mode = "high"
+      ]
+    ]
+  def index(self.index, page) {
+    pageTitle = "Feed",
+    topbar = for screen_content:
+      icon = "img.png":
+        data = for icon([width, height = (35, 70)]);
+        icon.register = [].RESOLUTION(texture_resolution = ["1080p"], graphics = ["high"]):
+          register()
+  }
+  feed = {
+    def feed(self):
+      feed === self.collect.feed(posts || topics || subscriptions):
+        collect.data = preference.coll()
+        preference.protcal = collect() {
+          data = protocol()===!{feed.collect}
+          posts || topics || subscriptions || accounts = True()
+        }
+        SHOWCASE.FEED {} FEED.DATA=True():
+          {} data {} feed:
+            showcase()
+  }
+  class customize_posts(self, posts) {
+    POST_WIDTH || POST_HEIGHT = [(26.5, 26.5)]:
+      graphics = mode("high") {
+        texture_resolution = "1080p";
+      }
+      post_data = [
+        content === self.collect {} post.content.messageID {} else {} if image == collect.image.dat() {} else {} if video = collect.video.dat()
+        image_data = if image=set(width && height = data[14.5, 14.5]);
+        video_data = if video=set(width && height = data[14.5, 14.5]);
+        text_data = if text=set(width && height = data[7.25, 7.25]);
+        data = set()
+        ]
+        above_post_content === addRow() {
+          row = row;
+          row_content = [
+            author_icon === [icon=[(round=True().icon)]]==icon();
+            icon_data = width && height[(10.5, 10.5)]:
+              icon = self.collect.icon.image(True):
+                collect_icon = pixels && images()
+            ]
+            display = left;()/position="fixed"==!True();
+            towards=right(//{self.username=ctx.author.username=True})=!data()
+            username_data = [color='bold', width && height=7.00]===!True((=)){}display(right(out=2.5(px)));
+            showcase = True()
+        }
+        def main_border(self, border) {
+          border = data[width && height === [(300, 125)]]==set((color=border_color({\self.collect{currentTheme}}=set(default{|white|==if(no_theme=True)})
+          border.content == setImage(display{left=[image="/assets/image_assets/vicegram-logo.png"]})=[data = imgWidth && imgHeight = (10.74px, 10.74px)]=set(//self.image=image)>=assetLoad=True()
+          })));
+        }
+        MULTIPLE.rows === collect.Feed:
+          def createPersonalizedFeed(self, feed) {
+            self.feed = manage()/data(True) {
+              personalizedfeed [
+                self.create()
+                self.rows = multiple()
+                base || content {} || of || following ():
+                  collect.following && collect.likes && collect.commentedOn && collect.Shared && collect.SavedPosts:
+                    collect = features()
+                    display in feed ()==!True;
+                ]
+                feed = create for user(True);
+                exec.feed = True()
+                FEED = FEED()
+            }
+            data = posts && videos && images:
+              collect()
+              personalize==!True() {
+                self.data = data()
+              }
+              feed_manage = data()
+              equiv()===!True()
+          }
+          feed = feed;
+  }
+  add = +{collectPostsFromFollowedUsers=True}[]>data() {
+    managed()==[update=feed(True)]
+  }
+}
